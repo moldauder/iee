121ce16a931255354d8c5bdcbe20c08bf86d89a1
cnruhua <cnhruhua@gmail.com>
2013-01-20 11:38:57 +0800

    remove dump

--- common/functions.php
+++ common/functions.php
@@ -320,30 +320,3 @@ function getURLTitle($url){
     return $url;
 }
 
-function dump($var, $label = null, $echo = true, $strict = true) {
-    $label = ($label === null) ? '' : rtrim($label) . ' ';
-    if(!$strict){
-        if(ini_get('html_errors')){
-            $output = print_r($var, true);
-            $output = '<pre>' . $label . htmlspecialchars($output, ENT_QUOTES) . '</pre>';
-        }else{
-            $output = $label . print_r($var, true);
-        }
-    }else{
-        ob_start();
-        var_dump($var);
-        $output = ob_get_clean();
-        if(!extension_loaded('xdebug')){
-            $output = preg_replace("/\]\=\>\n(\s+)/m", '] => ', $output);
-            $output = '<pre>' . $label . htmlspecialchars($output, ENT_QUOTES) . '</pre>';
-        }
-    }
-    if($echo){
-        echo($output);
-        return null;
-    }else{
-        return $output;
-    }
-}
-
-
