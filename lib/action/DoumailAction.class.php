ce347b848886606bc0374cd0110d27ff0eaa6ccf
cnruhua <cnhruhua@gmail.com>
2013-03-16 14:56:05 +0800

    add doumail files

--- lib/action/DoumailAction.class.php
+++ lib/action/DoumailAction.class.php
@@ -7,9 +7,87 @@ class DoumailAction extends AuthAction{
     private $authURL = 'https://www.douban.com/service/auth2/';
     private $apiURL = 'https://api.douban.com/v2/';
 
-    public function _empty(){
+    public function __construct(){
+        $this->checkLogin();
+        if(!IS_SUPER_USER){
+            exit;
+        }
     }
 
+    /**
+     * 列出所有豆邮活动
+     */
+    public function all(){
+        $biz = System::B('Doumail');
+        $list = $biz->findActs();
+
+        $this->assign('actList', $list);
+        $this->display();
+    }
+
+    /**
+     * 创建活动
+     */
+    public function create(){
+        $this->checkLogin();
+        $this->display();
+    }
+
+    /**
+     * 保存活动
+     */
+    public function saveact(){
+        $biz = System::B('Doumail');
+
+        $id =  System::filterVar($_POST['id']);
+        if($id && !$biz->findActById($id)){
+            $this->ajax(array(
+                'msg'     => '您要编辑的豆邮活动不存在',
+                'success' => false
+            ), 'json');
+        }
+
+        $title = System::filterVar($_POST['title']);
+        if(!$title){
+            $this->ajax(array(
+                'msg'     => '请填写豆邮标题',
+                'success' => false
+            ), 'json');
+        }
+
+        $content = System::filterVar($_POST['content']);
+        if(!$content){
+            $this->ajax(array(
+                'msg'     => '请填写豆邮内容',
+                'success' => false
+            ), 'json');
+        }
+
+        $data = array(
+            'name' => $title,
+            'content' => $content,
+            'closed' => 'n'
+        );
+
+        if(!$id){
+            $data['created'] = date('Y-m-d H:i:s');
+            $data['amount'] = 0;
+        }
+
+        $id = $id ? $biz->updateAct($id, $data) : $biz->addAct($data);
+
+        if(false !== $id){
+            $this->ajax(array(
+                'success' => true,
+                'id'      => $id
+            ), 'json');
+        }else{
+            $this->ajax(array(
+                'success' => false,
+                'msg'     => $biz->getDBError()
+            ), 'json');
+        }
+    }
 
     /**
      * 显示豆瓣授权页面
--- lib/biz/DoumailBiz.class.php
+++ lib/biz/DoumailBiz.class.php
@@ -1,4 +1,37 @@
 <?php
 class DoumailBiz extends Biz{
 
+    /**
+     * 查找全部的活动
+     */
+    public function findActs(){
+        $db = $this->getDBConnection();
+
+        $db->table('^doumail_act');
+        return $db->select();
+    }
+
+    /**
+     * 根据id获取活动信息
+     */
+    public function getActById($id){
+        return $this->getDBConnection()->table('^doumail_act')->where('id', $id)->selectOne();
+    }
+
+    /**
+     * 保存豆邮活动
+     */
+    public function addAct($data){
+        $db = $this->getDBConnection();
+        if($db->table('^doumail_act')->where('^doumail_act')->data($data)->add()){
+            return $db->getLastInsID();
+        }
+
+        return null;
+    }
+
+    public function updateAct($id, $data){
+        return $this->getDBConnection()->table('^doumail_act')->where('^doumail_act')->data($data)->where('id', $id)->save();
+    }
+
 }
